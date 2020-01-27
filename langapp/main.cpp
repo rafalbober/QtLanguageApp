ea90912024b79a18bec3207edc693cb0e577ad34
rafaubober <rafaubober@gmail.com>
2020-01-27 13:23:09 +0100

    Nothing Special

--- langapp/main.cpp
+++ langapp/main.cpp
@@ -1,32 +1,18 @@
-/*#include "mainwindow.h"
 #include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
-}*/
-
-#include <QApplication>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QUrl>
-#include <QtNetwork/QNetworkReply>
 #include <QDebug>
 #include "mainwindow.h"
+#include <QtSql/QSqlDatabase>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    QNetworkAccessManager* qnam = new QNetworkAccessManager();
-    QNetworkReply* rep = qnam->get(QNetworkRequest(QUrl("https://translate.google.com/?hl=pl#view=home&op=translate&sl=en&tl=pl&text=great")));
-    qDebug() << rep;
-    QString result = QVariant(rep->readAll()).toString();
-    qDebug() << result;
-    qnam->deleteLater();
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+      db.setHostName("bigblue");
+      db.setDatabaseName("flightdb");
+      db.setUserName("acarlson");
+      db.setPassword("1uTbSbAs");
+      bool ok = db.open();
     return a.exec();
 }
